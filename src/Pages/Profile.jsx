314116577727dc41536bbfc0ff209bb15c668c40
VarunORE9846 <Varun.Sharma@orionesolutions.in>
2024-02-02 18:17:34 +0530

    fourth commit

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,49 +1,96 @@
-import React, { useState } from 'react'
-import "react-alice-carousel/lib/alice-carousel.css";
-import '../Pages/styles.css';
+import React from 'react';
+import { useState, useEffect } from 'react';
 export const Profile = () => {
   const [name, setName] = useState("");
-  const [namee, setNamee] = useState("");
-  const [show, setShow] = useState(false);
+  const [age, setAge] = useState(0);
+  // const [show, setShow] = useState(false);
+  const [users, setUsers] = useState([]);
 
-  const handleChange = (e) => {
-    setName(e.target.value);
-    return;
+  const handleOnChangeValues = (e) => {
+    const { name, value } = e.target
+    if (name === "name") {
+      setName(value);
+    }
+    if (name === "age") {
+      setAge(value);
+    }
+
   };
-  const handleChhange = (e) => {
-    setNamee(e.target.value);
-    return;
-  };
-  const handleClick = () => {
-    if (name.length < 4) {
-      alert("please enter your name in field 1");
+  const handleClick = (e) => {
+    if (name.length < 2) {
+      alert("Please enter your name");
       return;
     }
-    if (namee.length < 4) {
-      alert("please enter your name in field 2");
-      return;
-    }
-    if (name === namee) {
-      alert("name entered in both fields is same")
-    }
-    else {
-      alert("name entered in both fields are not same")
+    if (age <= 0) {
+      alert("Please enter age correctly");
       return;
     }
 
-    setShow(!show)
+    localStorage.setItem("usersTable", JSON.stringify([...users, { name, age }]));
+    setUsers([...users, { name, age }])
+
+    setName("");
+    setAge(0);
   };
+  const handleDelete = (e) => {
+    console.log(e);
+    const temp = [...users]
+    let newArray = []
+    for (let index = 0; index < temp.length; index++) {
+      if (index == e) {
+        continue
+      } else {
+        newArray.push(temp[index])
+      }
+    }
+    console.log(newArray);
+    localStorage.setItem("usersTable", JSON.stringify(newArray));
+    setUsers(newArray)
+  };
+  const resetLocalStorage = () => {
+    localStorage.removeItem("usersTable");
+    setUsers([]);
+  };
+  useEffect((e) => {
+    // e.preventdefault();
+    const users = localStorage.getItem("usersTable");
+    console.log(users)
+    if (users) {
+      setUsers(JSON.parse(users));
+    }
+  }, []);
   return (
-    <>
-      <label htmlFor="name">Name:</label>
-      <input type="text" placeholder='enter your name' onChange={handleChange} />
-      <label htmlFor="name">Name:</label>
-      <input type="text" placeholder='enter your name' onChange={handleChhange} />
-      <button onClick={handleClick} >Click Me</button>
-      {show && <> <h2>{name}</h2>
-        <h2>{namee}</h2> </>}
-
-    </>
+    <div>
+      <label htmlFor="">Name:</label>
+      <input type="text" value={name} onChange={(e) => handleOnChangeValues(e)} name="name" placeholder='enter your name' />
+      <label htmlFor="">Age:</label>
+      <input type="number" value={age} onChange={(e) => handleOnChangeValues(e)} name="age" placeholder='enter your age' />
+      <button onClick={handleClick}>click Me</button>
+      <button onClick={resetLocalStorage}>Reset</button>
+      {users.length > 0 &&
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">S. No.</th>
+              <th scope="col">Name</th>
+              <th scope="col">age</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((e, i) => (
+              <tr key={`${i}_row`}>
+                <th scope="row">{i + 1}</th>
+                <td>{e?.name}</td>
+                <td>{e?.age}</td>
+                <td><button onClick={() => handleDelete(i)}>delete</button></td>
+              </tr>
+            ))
+            }
+          </tbody>
+        </table>
+      }
+      {/* <h3>{name}</h3> */}
+    </div>
   )
-};
+}
 
